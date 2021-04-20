5a482f15a55ff683e22be8fa648fd147efaa01b3
Paulo Franciscon <paulo.franciscon@icloud.com>
2021-04-20 17:35:11 -0300

    Atualizaçao da API da listagem de podcasts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,15 @@
+import { useEffect } from 'react'
+
 export default function Home() {
+  //função de gatilhos de eventos quando muda algo na aplicação, efeitos colaterais
+  //              { o que}, [Quando ]
+  useEffect(() => { 
+    fetch('http://localhost:3333/episodes')
+    .then(response => response.json())
+    .then(data => console.log(data) ) 
+  }, [])
+    
+
   return (
   <h1> Index </h1>
   )  
