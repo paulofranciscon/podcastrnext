fdcf414ee5abe6ce469d9bac98620a34302876b1
Paulo Franciscon <paulo.franciscon@icloud.com>
2021-04-20 18:15:09 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,21 @@
 import { useEffect } from 'react'
 
-export default function Home() {
-  //função de gatilhos de eventos quando muda algo na aplicação, efeitos colaterais
-  //              { o que}, [Quando ]
-  useEffect(() => { 
-    fetch('http://localhost:3333/episodes')
-    .then(response => response.json())
-    .then(data => console.log(data) ) 
-  }, [])
-    
-
+export default function Home(props) {
+  
   return (
   <h1> Index </h1>
   )  
 
 }
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8  //recarregar em server a cada 8 horas SSG
+  }
+} 
